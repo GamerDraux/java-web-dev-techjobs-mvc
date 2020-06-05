5d84b9ce8bce02259a720b69cefa4c56194e345f
Andy Koch <akandrewkoch@gmail.com>
2020-06-05 17:48:12 -0500

    Completed TODO tasks 3 and 4, and completed the 3 basic bonuses.  Have not tackled super bonus

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,19 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+    @RequestMapping(value="results")
+    public String displaySearchResults(Model model,
+                                       @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs = new ArrayList<>();
+        if (searchTerm.equals("all")||searchTerm.isBlank()){
+            jobs=JobData.findAll();
+        } else {
+            jobs=JobData.findByColumnAndValue(searchType,
+                    searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("lastSearch", searchTerm);
+        return "search";
+    }
 }
